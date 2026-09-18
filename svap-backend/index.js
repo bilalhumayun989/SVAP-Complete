e94051945ab2809052902453dc29b52410f593e5
bilalhumayun989 <bill.humayun1@gmail.com>
2026-09-18 13:04:32 +0500

    Change

--- svap-backend/index.js
+++ svap-backend/index.js
@@ -11,7 +11,7 @@ const orderRoutes = require('./routes/orderRoutes');
 const savedRoutes = require('./routes/savedRoutes');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5004;
 
 app.use(cors());
 app.use(express.json());
